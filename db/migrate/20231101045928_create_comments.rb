2454639433daec74d47c1acb70569abc2bae8e9c
Cristian Villa <c.villavirgen@gmail.com>
2023-10-31 23:23:52 -0600

    Remove the explicit t.datetime :created_at and t.datetime :updated_at declarations

--- db/migrate/20231101045928_create_comments.rb
+++ db/migrate/20231101045928_create_comments.rb
@@ -2,8 +2,6 @@ class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
       t.text :text
-      t.datetime :updated_at
-      t.datetime :created_at
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
 
--- db/migrate/20231101045935_create_likes.rb
+++ db/migrate/20231101045935_create_likes.rb
@@ -1,8 +1,6 @@
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
-      t.datetime :created_at
-      t.datetime :updated_at
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
 
