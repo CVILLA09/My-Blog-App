f7a024e47788445a7778faf56548298272555762
Cristian Villa <c.villavirgen@gmail.com>
2023-10-31 23:29:32 -0600

    Adjust Migration for Posts

--- db/migrate/20231101045913_create_posts.rb
+++ db/migrate/20231101045913_create_posts.rb
@@ -3,9 +3,9 @@ class CreatePosts < ActiveRecord::Migration[7.1]
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.references :author, null: false, foreign_key: true
-      t.integer :comments_counter
-      t.integer :likes_counter
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
 
       t.timestamps
     end
