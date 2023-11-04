48f8fe7eb199ea341f6043d9e2788ae970c71cfa
Cristian Villa <c.villavirgen@gmail.com>
2023-11-03 23:29:02 -0600

    Enhanced associations and methods in models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,16 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
 
-  # A method that updates the comments counter for a post
-  def update_comments_counter
-    post.increment!(:comments_counter)
+  # Attributes
+  attribute :text, :text
+
+  # Callbacks
+  after_create :update_post_comments_counter
+  after_destroy :update_post_comments_counter
+
+  # Methods
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-    # Attributes
-    attribute :title, :string
-    attribute :text, :text
-    attribute :comments_counter, :integer, default: 0
-    attribute :likes_counter, :integer, default: 0
-    
+  # Attributes
+  attribute :title, :string
+  attribute :text, :text
+  attribute :comments_counter, :integer, default: 0
+  attribute :likes_counter, :integer, default: 0
+
   # A method to update the posts counter for a user
   def update_posts_counter
     author.increment!(:posts_counter)
