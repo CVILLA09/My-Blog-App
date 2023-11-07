927aa9f39f90ddb190888929c790f00d0aeffe64
Cristian Villa <c.villavirgen@gmail.com>
2023-11-06 22:01:22 -0600

    Update PostsController to scope post finding to user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,24 @@
 class PostsController < ApplicationController
-  before_action :find_user, only: [:index]
-  before_action :find_post, only: [:show]
-
-  def index
-    @posts = @user.posts
+    before_action :find_user
+    before_action :find_post, only: [:show]
+  
+    def index
+      @posts = @user.posts
+    end
+  
+    def show; end
+  
+    private
+  
+    def find_user
+      @user = User.find(params[:user_id])
+    end
+  
+    def find_post
+      @post = @user.posts.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = 'Post not found. Redirecting to user posts.'
+      redirect_to user_posts_path(@user)
+    end
   end
-
-  def show; end
-
-  private
-
-  # This method will only be used by the index action
-  def find_user
-    @user = User.find(params[:user_id])
-  end
-
-  # This method will only be used by the show action
-  def find_post
-    @post = Post.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:alert] = 'Post not found. Redirecting to posts list.'
-    redirect_to posts_path
-  end
-end
+  
