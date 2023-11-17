f3f85f6ee2bf39516904208cd2eb6f705b64d228
Cristian Villa <c.villavirgen@gmail.com>
2023-11-17 13:31:35 -0600

    Add name field to Devise registration view and permit name parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # This method allows us to accept additional parameters for the user sign-up and account update processes.
+  def configure_permitted_parameters
+    # For sign-up process, permit the :name parameter along with the other required parameters.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    # For the account update process, permit the :name parameter along with other parameters that might be needed for updating the user's account.
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
