b9815c952fc434a66850beca5bd2296a8663d6a2
Cristian Villa <c.villavirgen@gmail.com>
2023-11-17 21:33:58 -0600

    Enhance user authentication flow and permit additional signup parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,10 @@ class ApplicationController < ActionController::Base
     # For the account update process, permit the :name parameter along with other parameters that might be needed for updating the user's account.
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  # This method determines where to redirect after a successful sign-in.
+  def after_sign_in_path_for(resource)
+    # Redirect to the user index page or any other path you want to send the user to after sign-in
+    users_path # or use 'user_path(current_user)' if you want to redirect to the user's show page
+  end
 end
