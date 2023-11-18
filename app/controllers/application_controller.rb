ad6e66a9e794e42b60afb4adcc6d1cc7941edc7a
Cristian Villa <c.villavirgen@gmail.com>
2023-11-18 15:47:59 -0600

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,20 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # This method allows us to accept additional parameters for the user sign-up and account update processes.
+  # This method allows us to accept additional parameters for the user
+  # sign-up and account update processes.
   def configure_permitted_parameters
-    # For sign-up process, permit the :name, :bio, and :photo parameters along with the other required parameters.
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :bio, :photo, :password, :password_confirmation])
+    # For sign-up, permit :name, :bio, :photo, and other required parameters.
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email bio photo password password_confirmation])
 
-    # For the account update process, permit the :name, :bio, and :photo parameters along with other parameters that might be needed for updating the user's account.
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :photo])
+    # For account updates, permit :name, :bio, :photo, and any other parameters
+    # that might be needed for updating the user's account.
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo])
   end
 
   # This method determines where to redirect after a successful sign-in.
-  def after_sign_in_path_for(resource)
-    # Redirect to the user index page or any other path you want to send the user to after sign-in
-    users_path # or use 'user_path(current_user)' if you want to redirect to the user's show page
+  def after_sign_in_path_for(_resource)
+    # Redirect to the user index page or any other path you want to send the user to after sign-in.
+    users_path # or user_path(current_user) to redirect to the user's show page.
   end
 end
